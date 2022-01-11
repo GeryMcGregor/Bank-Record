790825459b9e0d786dbf7429e994fcafdd843459
GeryMcGregor <gunamanta.yuana@gmail.com>
2022-01-11 21:31:31 +0800

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 using namespace std;
 
+bool account = false;
+float accountBalance = 0.0;
+
 void mainMenu();
 
 int main(int argc, char const *argv[])
@@ -11,7 +14,8 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void mainMenu(){
+void mainMenu()
+{
     int choice;
 
     cout << "Main Menu" << endl;
@@ -23,9 +27,70 @@ void mainMenu(){
     cout << "06. CLOSE AN ACCOUNT" << endl;
     cout << "07. MODIFY AN ACCOUNT" << endl;
     cout << "08. EXIT" << endl;
-    cout << "Type a number (1-8)" << endl;
+    cout << "Type a number (1-8): ";
 
     cin >> choice;
 
-    
+    switch (choice)
+    {
+    case 1:
+        if (account == true)
+        {
+            cout << "You already have an account !" << endl;
+            mainMenu();
+        }
+        else
+        {
+            account = true;
+            cout << "An account has been successfully created !" << endl;
+            cout << "Proceed to main menu..." << endl;
+            cin.get();
+            mainMenu();
+        }
+        break;
+    case 2:
+        if (account == true)
+        {
+            float transferValue;
+            cout << "Money to transfer: ";
+            cin >> transferValue;
+
+            accountBalance + transferValue = accountBalance;
+            cout << transferValue << "has been transferred to your account" << endl;
+            mainMenu();
+        }else {
+            cout << "Please make an account first !" << endl;
+            mainMenu();
+        }
+        break;
+    case 3:
+        if (account == true)
+        {
+            float transferValue;
+            
+        }
+        
+
+
+        break;
+    case 4:
+        // code block
+        break;
+    case 5:
+        // code block
+        break;
+    case 6:
+        // code block
+        break;
+    case 7:
+        // code block
+        break;
+    case 8:
+        return;
+        break;
+    default:
+        cout << "Type a number (1-8): " << endl;
+        mainMenu();
+        break;
+    }
 }
